65115667be4dba6416b6ba1a48bc9110b9360f63
andikajayaw <andika@sindata.net>
2024-12-13 15:02:40 +0800

    finishing quiz part1

--- quiz/part1/soal-3.js
+++ quiz/part1/soal-3.js
@@ -33,7 +33,7 @@ function makanTerusRekursif(waktu) {
 
 // TEST CASES
 console.log(makanTerusRekursif(66)); // 5
-// console.log(makanTerusRekursif(100)); // 7
-// console.log(makanTerusRekursif(90)); // 6
-// console.log(makanTerusRekursif(10)); // 1
-// console.log(makanTerusRekursif(0)); // 0
+console.log(makanTerusRekursif(100)); // 7
+console.log(makanTerusRekursif(90)); // 6
+console.log(makanTerusRekursif(10)); // 1
+console.log(makanTerusRekursif(0)); // 0
